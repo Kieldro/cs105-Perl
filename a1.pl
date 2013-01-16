f410e4087ba0df284791c2f88be0a608d0edf16f
Ian Buitrago <kieldro@gmail.com>
2013-01-16 13:15:48 -0600

    Prints properly.

---
+++ a1.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+print " _____________________________________
+/ The naked truth of it is, I have no \\
+| shirt.                              |
+|                                     |
+| -- William Shakespeare, \"Love's     |
+\\ Labour's Lost\"                      /
+ -------------------------------------
+   \\
+    \\
+        .--.
+       |o_o |
+       |:_/ |
+      //   \\ \\
+     (|     | )
+    /'\\_   _/`\\
+    \\___)=(___/
+";
