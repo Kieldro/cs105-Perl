98306ef445608a01bdeb144b067985847aade558
Ian Buitrago <kieldro@gmail.com>
2013-01-30 12:48:19 -0600

    more notes.

--- hello.pl
+++ hello.pl
@@ -3,7 +3,7 @@
 # questions
 # multiline
 # ./ in path?
-@a = qw(foo bar baz);
+@a = qw(foo bar baz);		# quote words, quote the fucking words, no commas or quotes!
 @a = ('foo', 'bar', 'baz');		# single quotes are raw
 
 while (defined($a = <STDIN>)){
@@ -18,7 +18,7 @@ while (<STDIN>){
 # 1-23-13
 # wc -l # word count number of lines
 
-while (<>){
+while (<>){		# "	while diamond"
 	# $_ constains end char
 	chomp;		# removes only line terminator, assumes $_
 	++$b;
@@ -37,8 +37,63 @@ given($grade){
 	default{ print "Always\n"; }
 	
 }
+# hashes
 %a = ('a', 1, 'b', 2, 'c', 3);
 %a = ('a' => 1, 'b' => 2, 'c' => 3);		# syntactic sugar/sugar
 
 
-die if true;
+die "usage: $0 sdldl\n" if true;
+
+foreach $foo(@foo){		# iterating over plurals
+	$foo .= $foo
+}
+
+foreach $foo (keys %foo){		# iterates over keys
+	$foo{} .= $foo
+}
+
+foreach $bar (values %foo){		# iterates over keys
+	print "$bar\n"
+}
+
+# C style for loops included
+# foreach and for are synonyms
+foreach (@baz){
+	# iterates into $_
+}
+
+# file IO
+open($config, 'config');
+while(<$config>){
+	chomp;
+}
+close$config;
+
+# 3 arg open is more secure
+open($config, '<', 'config');
+
+split		# into an array of tokens
+# $_ is implicit not magical or special. Documented in perlvar
+
+# $0 is auto set to name of program
+open or die "Could not open $!\n"		# $! is the error
+push @a, 5;		# push 5 onto the end of @a
+pop @a;		# pop the last element
+unshift @a, 1;		# idk
+shift @a;
+
+@a[1, 3];		# slice array
+($a, $b) = @a{'k1', 'k2'};		# slice hash
+# or assign to slices
+@a[1, -3] = qw(M W);
+
+# List Flattening
+@a = (@b, @c, $a, keys %d);
+# splice removes scalars from arrays?
+sort
+@ARGV
+
+
+printf
+my
+our
