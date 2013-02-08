848dcd624d2d55f2e64fe024b9602e15eea0fe95
Ian Buitrago <kieldro@gmail.com>
2013-02-07 23:52:51 -0600

    A4 complete.

--- calc.pl
+++ calc.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 # Ian Buitrago
 # 2-11-2013
-# log: 2-4 3 hrs
+# log: 10-12 2 hrs
 # Assignment 4: Reverse Polish Notation
 
 use warnings;
@@ -17,10 +17,10 @@ while(<>){
 	# operand
 	if(m/\d+/){ push @stack, $_ }		# no semicolon? 5.1?
 	# operators
-	elsif(/\+/){ add() }
-	elsif(/\-/){ minus() }
-	elsif(/\*/){ mult() }
-	elsif(/\//){ div() }
+	elsif(/\+/){ $stack[-2] += pop @stack; }
+	elsif(/\-/){ $stack[-2] -= pop @stack; }
+	elsif(/\*/){ $stack[-2] *= pop @stack; }
+	elsif(/\//){ $stack[-2] /= pop @stack; }
 	elsif(/squares/i){ sq() }
 	elsif(/sqrt/i){ root() }
 	elsif(/sum/i){ sum() }
@@ -31,34 +31,7 @@ while(<>){
 	say "Stack: @stack"
 }
 
-sub add{
-	my $b = pop @stack;
-	my $a = pop @stack;
-	
-	push @stack, $a + $b
-}
-
-sub minus{
-	my $b = pop @stack;
-	my $a = pop @stack;
-	
-	push @stack, $a - $b
-}
-
-sub mult{
-	my $b = pop @stack;
-	my $a = pop @stack;
-	
-	push @stack, $a * $b
-}
-
-sub div{
-	my $b = pop @stack;
-	my $a = pop @stack;
-	
-	push @stack, $a / $b
-}
-
+# special functions
 sub sq{ $stack[-1] *= $stack[-1] }
 
 sub root{ $stack[-1] = sqrt($stack[-1]) }
@@ -78,26 +51,9 @@ sub residuals{
 }
 
 sub stddev{
-	
-	say "debug: @stack" if $DEBUG;
-	
-	$stack[-1] *= $stack[-1]
+	residuals();
+	@stack = map {$_ * $_} @stack;		# square each element
+	mean();
+	root();
+	#say "debug: @stack" if $DEBUG;
 }
-
-
-
-=begin
-sqrt
-    Replace the top element of the stack with its square root. (You can make use of the built-in sqrt function.) 
-sum
-    Replace the stack with the sum of all the numbers that were on the stack. 
-mean
-    Replace the stack with the arithmetic mean of all the numbers that were on the stack. This value should be equal to the sum of all the numbers divided by the size of the stack. 
-squares
-    Multiply each number on the stack by itself. 
-residuals
-    Subtract from each number on the stack the mean of all the numbers on the stack. 
-stddev
-    Replace the stack with the standard deviation of all the numbers that were on the stack.
-    This value should be equal to the square root of the mean of the squares of the residuals of all the numbers on the stack. 
-=cut
