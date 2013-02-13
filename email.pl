930837dd4012ed65eefa1ef25332545d2cddc32d
Ian Buitrago <kieldro@gmail.com>
2013-02-13 16:47:51 -0600

    removed push array.

--- email.pl
+++ email.pl
@@ -3,6 +3,7 @@
 # 2-18-2013
 # log: 1330-1500 2.5 hrs
 # Assignment 5: Legitimate Business Purposes
+# without g flag, infinite loop?
 
 # use warnings; no warnings 'once';
 use v5.10;
@@ -15,16 +16,13 @@ while(<>){
 	$block = qr/[\w-]+/;
 	$emailRE = qr/$block(\.$block)*\@$block(\.$block)*/;
 	
-	if (m/$emailRE/){
-		push @emails, $& unless exists $count{$&};
-		++$count{$&};
-	}
+	++$count{$&} while (m/$emailRE/g);
 }
-# say $& if $DEBUG;
 
 printf "%i distinct address found.\n", scalar keys %count;
 
-# Sort
-@emails = sort {$count{$b} <=> $count{$a} || $a cmp $b} @emails;	# cmp is default sort
+# Sort by count then lexigraphcally
+@emails = sort {$count{$b} <=> $count{$a} || $a cmp $b} keys %count;	# cmp is default sort
+
 # Output
 map {say qq/$_ ($count{$_})/} @emails;
