39d1f34a216c735a7d3835edd35f563fa3650182
Ishigori Con <ttn846@gmail.com>
2013-03-23 01:46:18 -0500

    Fixed a few things, added movie.pl

--- final/Actor.pl
+++ final/Actor.pl
@@ -5,10 +5,7 @@ use v5.10;
 # create actor object that stores hash of actors
 sub new {
 	my $class = shift;
-	my $name = shift;
 	my %actors = {};
-	
-	# connecting actor to the name and list of movies
 	my $self = \%actors;
 	
 	bless $self, %actors, $class;
