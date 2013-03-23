9a0f52daa12656b0963a9a7c15e7710e347e161b
Ian Buitrago <kieldro@gmail.com>
2013-03-23 16:34:06 -0500

    zcat works.

--- final/final.pl
+++ final/final.pl
@@ -10,15 +10,15 @@
 # Final Project: The Six Degrees of Kevin Bacon
 
 use v5.10;
-use actor;
-use movie;
+# use Actor;
+# use Movie;
 
 $DEBUG = 1;
 
 # Graph with Nodes as Actors and Edges as Movies
 # Cycles will be prevented through "discovering" nodes
-my $actorList = Actor->new();
-my $movieList = Movie->new();
+# my $actorList = Actor->new();
+# my $movieList = Movie->new();
 
 
 # Preload data into hashes
@@ -34,47 +34,47 @@ $movieRE = qr/\t+(?<movie>(?!")\w+.+(?!")\ $yearRE(?!\(TV|V|VG\)))/x;
 # if found then we add an edge between the two actors
 
 # Split regex into smaller pieces
-# foreach my $inFile (@ARGV) {
-# 	say "Opening $inFile...";
-# 	open IN, "zcat $inFile|" or die "Could not open $inFile: $!";
-while(<>){
-	chomp;
-	
-	if(/$actorRE $titleRE/x){
-	# say 'GOOM' if '	test (2013)' =~ /$movieRE/;
-		$key = $+{actor};
-		say STDERR '$key: '.$key if $DEBUG;
-		$title = $+{title};
-			say STDERR '$title: '.$title;
-		if($title =~ /$movieRE/){
-			say STDERR '$title: '.$+{movie};
+foreach my $inFile (@ARGV) {
+	say "Opening $inFile...";
+	open IN, "zcat $inFile |" or die "Could not open $inFile: $!";
+	while(<IN>){
+		chomp;
+		
+		if(/$actorRE $titleRE/x){
+		# say 'GOOM' if '	test (2013)' =~ /$movieRE/;
+			$key = $+{actor};
+			say STDERR '$key: '.$key if $DEBUG;
+			$title = $+{title};
+				say STDERR '$title: '.$title;
+			if($title =~ /$movieRE/){
+				say STDERR '$title: '.$+{movie};
+			}
+		}elsif(/$movieRE/){
+			$movie = $+{movie};
+			# say STDERR '$movie: '.$movie if $DEBUG;
 		}
-	}elsif(/$movieRE/){
-		$movie = $+{movie};
-		# say STDERR '$movie: '.$movie if $DEBUG;
 	}
-	
 }
 
 # should have list of actors and movies by now, perform 
 # "bacon number computation"
 
-%baconNumbers = findBaconNumbers($actorList);
+# %baconNumbers = findBaconNumbers($actorList);
 
-sub findBaconNumbers {
-	my $actorList = shift;
-	%baconNumbers = {};
-	# start from people with Bacon in their name
-	my @bacons = $actorList->searchActors("bacon");
-	# grab the movie list from each bacon 
-	while(@bacons) {
-		$actor = Actor->new();
-		$baconPerson = shift(@bacons);
-		@baconMovies = $actor->getMoviesOfActor($baconPerson);
+# sub findBaconNumbers {
+# 	my $actorList = shift;
+# 	%baconNumbers = {};
+# 	# start from people with Bacon in their name
+# 	my @bacons = $actorList->searchActors("bacon");
+# 	# grab the movie list from each bacon 
+# 	while(@bacons) {
+# 		$actor = Actor->new();
+# 		$baconPerson = shift(@bacons);
+# 		@baconMovies = $actor->getMoviesOfActor($baconPerson);
 		
-	}
-	return %baconNumbers;
-}
+# 	}
+# 	return %baconNumbers;
+# }
 
 # # Input from user
 # while(<>){
