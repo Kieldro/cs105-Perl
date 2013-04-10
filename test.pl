4659c38956d69e59a5358270610be9823b5e6d5f
Ian Buitrago <kieldro@gmail.com>
2013-04-10 12:34:38 -0500

    Adds a sweet 1 liner script.

--- test.pl
+++ test.pl
@@ -1,29 +1,33 @@
 #!/usr/bin/perl
 # searches network for a user
 
-use v5.10;
+# use v5.10;
 
-$user = 'jwilke';
-@machines = `cshosts pub`;
-say "searching for $user...";
+# $user = 'jwilke';
+# @machines = `cshosts pub`;
+# say "searching for $user...";
 
-foreach $host (@machines){
-	spin();
-	chomp $host;
-	@input = `finger \@$host`;
-	foreach(@input){
-		say "\n$user found on $host." and exit if /^($user)/;
-	}
-}
-say "\n$user not found.";
+# foreach $host (@machines){
+# 	spin();
+# 	chomp $host;
+# 	# say $host;
+# 	@input = `finger \@$host`;
+# 	foreach(@input){
+# 		say "\n$user found on $host." and exit if /^($user)/;
+# 	}
+# }
+# say "\n$user not found.";
 
-sub spin{
-	# my $oActor = ord $actor;
-	# return if $oActor < ord 'A' or $oActor > ord 'Z' or $oActor == $oLast;
-	# $oLast = ord $actor;
-	my $percent = (++$i) / (scalar @machines);
-	my $total = 25;
-	my $nEqs = int($percent * $total);
-	my $nSpaces = $total - $nEqs;
-	print STDERR "[".('=' x $nEqs).(' ' x $nSpaces)."]\r";
-}
+# sub spin{
+# 	# my $oActor = ord $actor;
+# 	# return if $oActor < ord 'A' or $oActor > ord 'Z' or $oActor == $oLast;
+# 	# $oLast = ord $actor;
+# 	my $percent = (++$i) / (scalar @machines);
+# 	my $total = 25;
+# 	my $nEqs = int($percent * $total);
+# 	my $nSpaces = $total - $nEqs;
+# 	print STDERR "[".('=' x $nEqs).(' ' x $nSpaces)."]\r";
+# }
+
+# golfed to 1 line
+#perl -e for$host(`cshosts pub`){for(`finger \@$host`){print "$host\n" and exit if /^keo/}}
